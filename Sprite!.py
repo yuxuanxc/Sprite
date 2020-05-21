8eadc8ab08e02e74dcc1f26260443fca8489906b
yuxuan830 <65273922+yuxuan830@users.noreply.github.com>
2020-05-21 14:03:06 +0800

    Update Sprite!.py

--- Sprite!.py
+++ Sprite!.py
@@ -16,9 +16,13 @@ image_path = os.path.join(current_path, 'images')
 def image(image):
     return pygame.image.load(os.path.join(image_path, image + '.png'))
 
-top_left_x = 100
-top_left_y = 150
+current_room = 1
 
+top_left_x = 0
+top_left_y = 100
+
+ROOM_WIDTH = 23
+ROOM_HEIGHT  = 16
 TILE_SIZE = 30
 
 player_y, player_x = 2, 5
@@ -48,29 +52,101 @@ player_frame = 0
 player_image = PLAYER[player_direction][player_frame]
 player_offset_x, player_offset_y = 0, 0
 
+#MAP#
+
+GAME_MAP = [["Room 0 - where unused objects are kept", 0, 0, False, False]]
+
+GAME_MAP  += [
+    #["Room name", Top exit?, Right exit?]
+    ["Raccoon House", False, False] #room 1
+    ]
+
+#OBJECTS#
+
+objects = {
+    0: [image('grass')],
+    1: [image('wall')],
+    2: [image('stones')],
+    3: [image('raccoon')],
+    4: [image('raccoonhouse')],
+    5: [image('wall')]
+    }
+
+items_player_may_stand_on = [0]
+
+#SCENERY#
+
+scenery = {
+    #room number: [[object number, y position, x position]...]
+    1: [[3, 4, 9], [4, 3, 11]]
+    }
+
+#MAKE MAP#
+
+def generate_map():
+    global room_map, top_left_x, top_left_y
+    room_data = GAME_MAP[current_room]
+    
+    floor_type = 0
+
+    room_map = [[floor_type] * ROOM_WIDTH]
+    
+    for y in range(ROOM_HEIGHT):
+        room_map.append([floor_type] * ROOM_WIDTH)
+
+    if current_room in scenery:
+        for this_scenery in scenery[current_room]: 
+            scenery_number = this_scenery[0]
+            scenery_y = this_scenery[1]
+            scenery_x = this_scenery[2]
+            room_map[scenery_y][scenery_x] = scenery_number
+
+            image_here = objects[scenery_number][0]
+            image_width = image_here.get_width()
+            image_width_in_tiles = int(image_width / TILE_SIZE)
+
+            for tile_number in range(1, image_width_in_tiles):
+                room_map[scenery_y][scenery_x + tile_number] = 255    
+    
 #DISPLAY#
 def draw_image(image, y, x):
     screen.blit(
         image,
-        (top_left_x + (x * TILE_SIZE),
-         top_left_y + (y * TILE_SIZE) - image.get_height())
+        (int(top_left_x + (x * TILE_SIZE)),
+         int(top_left_y + (y * TILE_SIZE) - image.get_height()))
         )
 
 def draw_player():
     player_image = PLAYER[player_direction][player_frame]
-    draw_image(player_image, player_y + player_offset_y, player_x + player_offset_x)
+    draw_image(player_image, player_y + player_offset_y,
+               player_x + player_offset_x)
     
 def draw():
     if game_over:
         return
 
-    pygame.draw.rect(screen, (0,0,0), (0, 0, 690, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (0, 100, 690, 450))
-    pygame.draw.rect(screen, (0,0,0), (0, 550, 690, 100))
+    pygame.draw.rect(screen, (0, 128, 0), (0, 100, 690, 450))
 
-    for y in range(HEIGHT):
+    screen.set_clip((0, 100, 690, 450))
+
+    floor_type = 0
+
+    for y in range(ROOM_HEIGHT):
+        for x in range(ROOM_WIDTH):
+            draw_image(objects[floor_type][0], y, x)
+
+    for y in range(ROOM_HEIGHT):
+        for x in range(ROOM_WIDTH):
+            item_here = room_map[y][x]
+            # Player cannot walk on 255: it marks spaces used by wide objects.
+            if item_here not in items_player_may_stand_on + [255]:
+                image = objects[item_here][0]
+                draw_image(image, y, x)
+        
         if (player_y == y):
             draw_player()
+
+    screen.set_clip(None)
     
 #mainloop#
 clock = pygame.time.Clock()
@@ -84,11 +160,9 @@ while run:
     clock.tick(11) 
     pygame.time.delay(40)
 
-    keys = pygame.key.get_pressed()    
+    generate_map()
     draw()
     
-    if game_over: break
-        
     if player_frame > 0:
         player_frame += 1
         #time.sleep(0.05)
@@ -96,13 +170,12 @@ while run:
             player_frame = 0
             player_offset_x = 0
             player_offset_y = 0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
     
     # save player's current position
     old_player_x = player_x
     old_player_y = player_y
+
+    keys = pygame.key.get_pressed()
     
     # move if key is pressed
     if player_frame == 0:
@@ -131,6 +204,11 @@ while run:
             player_direction = "down"
             player_frame = 1
 
+    if room_map[player_y][player_x] not in items_player_may_stand_on:
+        player_x = old_player_x
+        player_y = old_player_y
+        player_frame = 0
+    
     if player_direction == "right" and player_frame > 0:
         player_offset_x = -1 + (0.25 * player_frame)
     if player_direction == "left" and player_frame > 0:
@@ -143,4 +221,3 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
