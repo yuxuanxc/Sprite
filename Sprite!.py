269e92871b1bc4f319bfb5895f442251f456eb05
yuxuan830 <65273922+yuxuan830@users.noreply.github.com>
2020-05-23 12:21:55 +0800

    Update Sprite!.py

--- Sprite!.py
+++ Sprite!.py
@@ -21,7 +21,7 @@ def sound(sound):
     soundObj = pygame.mixer.Sound(os.path.join(sound_path, sound + '.wav'))
     soundObj.play()
 
-current_room = 2
+current_room = 1
 
 top_left_x = 0
 top_left_y = 100
@@ -62,18 +62,36 @@ WHITE = (255, 255, 255)
 
 #MAP#
 
-GAME_MAP = [["Room 0 - where unused objects are kept", 0, 0, False, False]]
+MAP_WIDTH = 4
+MAP_HEIGHT = 4
+MAP_SIZE = MAP_WIDTH * MAP_HEIGHT
 
-GAME_MAP  += [
-    #["Room name", Top exit?, Right exit?]
-    ["Raccoon House", False, False], #room 1
-    ["Raccoon Treehouse", False, True] #room 2
+GAME_MAP = [["Room 0 - where unused objects are kept"]]
+
+GAME_MAP += [
+    #["Room name"]
+    ["Base of Mountain"], #room 1
+    ["Jungle"], #room 2
+    ["Jungle"], #room 3
+    ["Raccoon Workshop"], #room 4
+    ["Raccoon Treehouse"], #room 5
+    ["Raccoon House"], #room 6
+    ["Jungle"], #room 7
+    ["Beach"], #room 8
+    ["Jungle"], #room 9
+    ["Jungle"], #room 10
+    ["Jungle"], #room 11
+    ["Beach"], #room 12
+    ["Where the jungle meets the beach"], #room 13
+    ["Beach"], #room 14
+    ["Beach"], #room 15
+    ["Beach"] #room 16
     ]
 
 #OBJECTS#
 
 objects = {
-    #Image, Shadow, Description
+    #Object number : [Image, Shadow, Description]
     0: [image('grass'), None, None],
     1: [image('wall'), None, "Wooden walls"],
     2: [image('wall1'), None, "Wooden walls"],
@@ -83,22 +101,34 @@ objects = {
     6: [image('longstones'), None, "Stones"],
     7: [image('raccoon'), None, "A friendly raccoon"],
     8: [image('fireplace'), None, "A charcoal fireplace"],
-    9: [image('charcoal'), None, "Some charcoal"],
-    10: [image('picture'), None, "A picture of a shiba inu"],
-    11: [image('charcoal'), None, "Some charcoal"],
-    12: [image('treehouse'), None, "A treehouse, with a ladder made from wooden planks"],
+    9: [image('charcoal'), None, "Some charcoal", "charcoal"],
+    10: [image('picture'), None, "A picture of a shiba inu", "picture"],
+    11: [image('ironore'), None, "Iron ore", "iron ore"],
+    12: [image('treehouse'), None, "A treehouse, \
+with a ladder made from wooden planks"],
     13: [image('baby-raccoon'), None, "A baby raccoon"],
-    14: [image('tree'), None, "A tree with blue leaves"]
+    14: [image('tree'), None, "A tree with blue leaves"],
+    15: [image('mountain'), None, "The base of a mountain"],
+    16: [image('ironore'), None, "Something shiny"],
+    17: [image('pickaxe'), None, "Pickaxe", "pickaxe"]
     }
 
-items_player_may_carry = list(range(9, 12))
+items_player_may_carry = list(range(9, 12)) + [17]
 items_player_may_stand_on = items_player_may_carry + [0]
 
 #SCENERY#
 
 scenery = {
     #room number: [[object number, y position, x position]...]
-    1: [[1, 5, 11], [1, 6, 11],
+    1: [[14, 4, 7], [14, 4, 10], [14, 4, 13], [14, 4, 16], [14, 5, 19],
+        [14, 15, 7], [14, 15, 10], [14, 15, 13], [14, 15, 16], [14, 14, 19],
+        [4, 5, 6], [4, 6, 6], [4, 7, 6], [4, 8, 6], [4, 9, 6],
+        [4, 10, 6], [16, 11, 6], [4, 12, 6], [4, 13, 6], [4, 14, 6],
+        [15, 15, 0]],
+    5: [[5, 6, 5], [5, 6, 8], [5, 6, 11], [5, 6, 14], [14, 6, 1],
+        [14, 6, 18], [5, 7, 0], [5, 7, 2], [5, 7, 18], [5, 7, 19],
+        [12, 7, 6], [13, 7, 17]],
+    6: [[1, 5, 11], [1, 6, 11],
         [2, 10, 11], [1, 11, 11], [1, 12, 11], [1, 5, 20], [1, 6, 20],
         [1, 7, 20], [1, 8, 20], [1, 9, 20], [1, 10, 20], [1, 11, 20],
         [1, 12, 20], [3, 4, 11], [3, 13, 11], [5, 6, 0], [5, 10, 0],
@@ -106,23 +136,24 @@ scenery = {
         [4, 12, 3], [4, 13, 3], [4, 14, 3], [6, 1, 3], [6, 15, 3],
         [4, 2, 22], [4, 3, 22], [4, 4, 22], [4, 5, 22],[4, 6, 22],
         [4, 7, 22], [4, 8, 22], [4, 9, 22], [4, 10, 22], [4, 11, 22],
-        [4, 12, 22], [4, 13, 22], [4, 14, 22], [7, 6, 9], [8, 4, 6]],
-    2: [[5, 6, 5], [5, 6, 8], [5, 6, 11], [5, 6, 14], [14, 6, 1], [14, 6, 18], 
-        [5, 7, 0], [5, 7, 2], [5, 7, 18], [5, 7, 19], [12, 7, 6], [13, 7, 17]]
+        [4, 12, 22], [4, 13, 22], [4, 14, 22], [7, 6, 9], [8, 4, 6]]
     }
 
 #PROPS#
 
 props = {
     #object number: [room, y, x]
-    9: [1, 4, 6],
+    9: [6, 4, 6],
     10: [0, 0, 0],
-    11: [1, 6, 6]
+    11: [0, 0, 0]
     }
 
-in_my_pockets = [10]
+in_my_pockets = [10, 17]
 selected_item = 0
 item_carrying = in_my_pockets[selected_item]
+
+RECIPES = [
+    ]
 
 def find_object_start_x():
     checker_x = player_x
@@ -141,7 +172,7 @@ def pick_up_object():
     if item_player_is_on in items_player_may_carry:
         room_map[player_y][player_x] = get_floor_type()
         add_object(item_player_is_on)
-        show_text("Now carrying ", 0)
+        show_text("Now carrying " + objects[item_player_is_on][3], 0)
         sound('pickup')
         pygame.time.delay(300)
     else:
@@ -156,7 +187,7 @@ def add_object(item):
     props[item][0] = 0
 
 def display_inventory():
-    pygame.draw.rect(screen, (0, 0, 0), (0, 550, 690, 100))
+    pygame.draw.rect(screen, BLACK, (0, 550, 690, 100))
 
     if len(in_my_pockets) == 0:
         return
@@ -173,7 +204,7 @@ def display_inventory():
     box_left = (selected_marker * 46) - 3
     pygame.draw.rect(screen, WHITE, (22 + box_left, 555, 40, 40), 3)
     item_highlighted = in_my_pockets[selected_item]
-    description = 'description'
+    description = objects[item_highlighted][2]
 
     myfont = pygame.font.SysFont('Arial', 20)
     textsurface = myfont.render(description, False, WHITE)
@@ -187,7 +218,7 @@ def drop_object(old_y, old_x):
         props[item_carrying][1] = old_y
         props[item_carrying][2] = old_x
         room_map[old_y][old_x] = item_carrying
-        show_text("You have dropped ", 0)
+        show_text("You have dropped " + objects[item_carrying][3], 0)
         sound('drop')
         remove_object(item_carrying)
         pygame.time.delay(300)
@@ -220,7 +251,7 @@ def examine_object():
                 and details[2] == left_tile_of_item
                 and room_map[details[1]][details[2]] != prop_number):
                 add_object(prop_number)
-                description = "You found "
+                description = "You found " + objects[prop_number][3]
                 sound('combine')
     show_text(description, 0)
     pygame.time.delay(300)
@@ -228,7 +259,7 @@ def examine_object():
 #USE OBJECTS#
 
 def use_object():
-    global room_map, props, item_carrying, selected_item, energy
+    global room_map, props, item_carrying, selected_item
     global in_my_pockets, game_over
 
     use_message = "You fiddle around with it but don't get anywhere."
@@ -240,6 +271,29 @@ def use_object():
     for this_item in [item_player_is_on, item_carrying]:
         if this_item in standard_responses:
             use_message = standard_responses[this_item]
+
+    if item_carrying == 17 and item_player_is_on == 16: #use pickaxe
+        use_message = "You found iron ore!"
+        add_object(11)
+        sound('combine')
+
+    for recipe in RECIPES:
+        ingredient1 = recipe[0]
+        ingredient2 = recipe[1]
+        combination = recipe[2]
+        if (item_carrying == ingredient1
+            and item_player_is_on == ingredient2) \
+            or (item_carrying == ingredient2
+                and item_player_is_on == ingredient1):
+            use_message = "You combine " + objects[ingredient1][3] \
+                          + " and " + objects[ingredient2][3] \
+                          + " to make " + objects[combination][3]
+            if item_player_is_on in props.keys():
+                props[item_player_is_on][0] = 0
+                room_map[player_y][player_x] = get_floor_type()
+            in_my_pockets.remove(item_carrying)
+            add_object(combination)
+            sound('combine')
 
     show_text(use_message, 0)
     pygame.time.delay(500)
@@ -288,13 +342,13 @@ def generate_map():
 #GAME LOOP#
                 
 def start_room():
-    show_text("You are here: " + room_name, 0)
+    show_text("You are here: " + GAME_MAP[current_room][0], 0)
 
 def game_loop():
     global player_x, player_y, current_room
     global from_player_x, from_player_y
     global player_image, player_image_shadow 
-    global selected_item, item_carrying, energy 
+    global selected_item, item_carrying
     global player_offset_x, player_offset_y
     global player_frame, player_direction
     
@@ -338,6 +392,41 @@ def game_loop():
             player_y += 1
             player_direction = "down"
             player_frame = 1
+
+    if player_x == ROOM_WIDTH: #goes through door on the right
+        current_room += 1
+        generate_map()
+        player_x = 0
+        player_y = int(ROOM_HEIGHT / 2)
+        player_frame = 0
+        start_room()
+        return
+
+    if player_x == -1: #goes through door on the left
+        current_room -= 1
+        generate_map()
+        player_x = ROOM_WIDTH - 1
+        player_y = int(ROOM_HEIGHT / 2)
+        player_frame = 0
+        start_room()
+        return
+
+    if player_y == ROOM_HEIGHT: #goes through door at the bottom
+        current_room += MAP_WIDTH
+        generate_map()
+        player_y = 0
+        player_x = int(ROOM_WIDTH / 2)
+        player_frame = 0
+        start_room()
+        return
+    
+    if player_y == -1: #goes through door at the top
+        current_room -= MAP_WIDTH
+        generate_map()
+        player_y = ROOM_HEIGHT - 1
+        player_x = int(ROOM_WIDTH / 2)
+        start_room()
+        return
 
     if keys[pygame.K_g]:
         pick_up_object()
@@ -437,8 +526,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
            
-    clock.tick(16)
-    pygame.time.delay(10)
+    clock.tick(28)
 
     generate_map()
     draw()
